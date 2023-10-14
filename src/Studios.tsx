0ca37d57d9587364f3b91ffe6e82aeabad4907e3
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-10-14 15:21:39 +0200

    Create studio selection UI

---
+++ src/Studios.tsx
@@ -0,0 +1,64 @@
+import { useAtom } from "jotai";
+import { studiosAtom } from "./atoms";
+
+export function Studios() {
+  const [studios, setStudios] = useAtom(studiosAtom);
+
+  const handleInput = (target: EventTarget, index: number) => {
+    const element = target as HTMLInputElement;
+    const value = Number(element.value);
+    if (value.toString() !== element.value) {
+      element.value = studios[index].toString();
+    }
+    setStudios([
+      ...studios.slice(0, index),
+      value,
+      ...studios.slice(index + 1),
+    ]);
+  };
+
+  const handleNewStudio = () => {
+    const newStudios = [...studios];
+    newStudios.push(1234);
+    setStudios(newStudios);
+  };
+
+  const handleStudioDelete = (index: number) => {
+    setStudios([...studios.slice(0, index), ...studios.slice(index + 1)]);
+  };
+
+  return (
+    <div class="flex flex-col items-end gap-2">
+      <h2 class="text-xl font-bold">Studios: {studios.length}</h2>
+      {studios.length > 0 ? (
+        <ul class="contents">
+          {studios.map((studio, index) => (
+            <li class="flex w-full gap-2" key={index}>
+              <input
+                class="w-full shrink rounded-xl bg-stone-300 px-2 py-1"
+                type="number"
+                value={studio}
+                onChange={(e) =>
+                  handleInput(e.target as HTMLInputElement, index)
+                }
+              />
+              <button
+                onClick={(e) => handleStudioDelete(index)}
+                class="font-bold text-sky-600 hover:underline"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+      <button
+        class="font-bold text-sky-600 hover:underline"
+        type="button"
+        onClick={handleNewStudio}
+      >
+        New
+      </button>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { render } from "preact";
+import { Studios } from "./Studios";
 import "./index.css";
 
 export function App() {
   return (
     <div>
-      <header class="flex items-center gap-2">
+      <header class="mb-2 flex items-center gap-2">
         <h1 class="text-3xl font-bold">Communistudio</h1>
         <nav class="font-bold text-sky-600">
           <a
@@ -15,6 +16,12 @@ export function App() {
           </a>
         </nav>
       </header>
+      <div class="flex gap-2">
+        <div class="w-48">
+          <Studios />
+        </div>
+        <div class="flex-grow bg-red-500/20">(placeholder for comments)</div>
+      </div>
     </div>
   );
 }
