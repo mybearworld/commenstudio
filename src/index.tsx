876bcf05f511a1ca924d06bd4f2e14fb4480fded
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-10-14 12:56:03 +0200

    Add header + a bit more styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,21 @@ import { render } from "preact";
 import "./index.css";
 
 export function App() {
-  return <div></div>;
+  return (
+    <div>
+      <header class="flex gap-2 items-center">
+        <h1 class="font-bold text-3xl">Communistudio</h1>
+        <nav class="text-sky-600 font-bold">
+          <a
+            href="https://github.com/mybearworld/communistudio"
+            class="hover:underline"
+          >
+            Github
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById("app"));
