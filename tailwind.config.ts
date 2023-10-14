cf7c9968afe022fb36154236a10c479e80897123
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-10-14 12:46:13 +0200

    More styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
